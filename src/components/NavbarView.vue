e8daf50c35371d572bc80569ab5193165a59fb94
federico-palma <fedepal9@gmail.com>
2022-02-25 13:05:55 +0100

    Added Burger menu on resize

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -5,12 +5,12 @@
                     <img id="logo" src="../assets/RickAndMortyLogo.png" alt="Rick and Morty logo"/>
                 </router-link>
             </div>
-            <!-- <div id="hamburger" onClick={ toggleHamburger } className={ isHamMenuOpen ? 'open-burger' : null }>
+            <div id="hamburger" @click="toggleBurger" :class="isMenuOpen ? 'open-burger' : null">
                 <div className="bar1"></div>
                 <div className="bar2"></div>
                 <div className="bar3"></div>
-            </div> -->
-            <div class="navbar-links" >
+            </div>
+            <div class="navbar-links" :class="isMenuOpen ? 'open-menu' : 'closed-menu'">
                 <router-link :to="{ name: 'characters' }">
                     <p id="nav-link-character">Characters</p>
                 </router-link>
@@ -26,6 +26,16 @@
 
 <script>
 export default {
+    data() {
+        return {
+            isMenuOpen: false
+        }
+    },
+    methods: {
+        toggleBurger() {
+            this.isMenuOpen = !this.isMenuOpen
+        }
+    }
 
 }
 </script>
@@ -70,5 +80,78 @@ export default {
 .navbar-links a:hover{
     background-color: #318895;
     border-radius: 5px;
+}
+
+#hamburger {
+    display: none;
+    position: absolute;
+    right: 50px;
+    top: 20px;
+    cursor: pointer;
+}
+
+.bar1, .bar2, .bar3 {
+    position: relative;
+    right: 10px;
+    width: 35px;
+    height: 5px;
+    background-color: rgb(240, 240, 240);
+    margin: 6px 0;
+    transition: 0.4s;
+  }
+
+/* Rotate first bar */
+.open-burger .bar1 {
+    -webkit-transform: rotate(-45deg) translate(-9px, 6px) ;
+    transform: rotate(-45deg) translate(-9px, 7px) ;
+}
+
+/* Fade out the second bar */
+.open-burger .bar2 {
+    opacity: 0;
+}
+
+/* Rotate last bar */
+.open-burger .bar3 {
+    -webkit-transform: rotate(45deg) translate(-8px, -8px) ;
+    transform: rotate(45deg) translate(-8px, -7px) ;
+}
+
+@media only screen and (max-width: 800px) {
+    #hamburger {
+        display: block;
+    }
+
+    .navbar-links {
+        width: 195px;
+        height: fit-content;
+        position: relative;
+        top: 85px;
+        right: 5px;
+        flex-direction: column;
+        background-color: #276a75;
+        border: solid 2px #1c4c53;
+        border-radius: 5px;
+        /* transition: 0.3s; */
+    }
+
+    .navbar-links a {
+        border-top: solid 1px #30828f;
+        border-bottom: solid 1px #30828f;
+    }
+
+    .navbar-links a:hover{
+        background-color: #318895;
+    }
+    
+    .closed-menu {
+        transform: translate(200px, 0px);
+        /* transition: 0.1s; */
+    }
+
+    .open-menu {
+        transform: rotate(0deg) translate(10px, 0px);
+        transition: 0.3s ease-out;
+    }
 }
 </style>
